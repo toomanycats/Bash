016dcf8afe687b69000ffe40caefd42c58669b7e
toomanycats <dpcuneo@fastmail.fm>
2016-08-16 20:38:01 -0700

    added custom script for opening a termianl on home laptop

--- backup_script.sh
+++ backup_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-rsync -hua --stats --progress --exclude anaconda --exclude .cache --exclude nltk_data  ~ /media/daniel/BACKUPFAT32/System76HomeBU
+rsync -hua --stats --progress --exclude git* --exclude ".*_history" --exclude anaconda --exclude .cache --exclude nltk_data  ~ /media/daniel/BACKUPFAT32/System76HomeBU
 
-exit 0
