04fcdee71117119279d479e3f03862de0aecddb1
Daniel P Cuneo <dpcuneo@arch04.als>
2018-05-03 14:20:29 -0700

    re-written: tries `kill, kill -HUP, kill -9`

--- zombie_killer.sh
+++ zombie_killer.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-kill -HUP $(ps -A -ostat,ppid | grep -e '[zZ]'| awk '{ print $2 }')
+function success
+{
+if [ $? -eq 0 ]; then
+    print "successful"
+    echo "true"
+else
+    echo "false"
+fi
+}
+
+function get_zombie_parents
+{
+    # newline delimited output
+    pids=$(ps -A -ostat,ppid | grep -e '[zZ]'| awk '{ print $2 }')
+    echo ${pids}
+}
+
+pids=$(get_zombie_parents)
+if [ -z ${pids} ]; then
+    echo "No zombie processes found."
+    exit 0;
+fi
+
+while read pid; do
+    kill -HUP ${pid} || kill ${pid} || kill -9 ${pid}
+    outcome=$(success)
+    if [ ${outcome} = "true" ];then
+        echo "${pid} brought down."
+    else
+        echo "${pid} failed to terminate."
+    fi
+done <<< $(get_zombie_parents)
